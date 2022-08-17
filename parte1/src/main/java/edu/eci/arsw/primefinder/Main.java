c6b59be027e72eebf81162fd0e4ecc70b896cc83
unknown <juan.cortes-p@labinfo.is.escuelaing.edu.co>
2022-08-17 09:38:02 -0500

    parte 1.1

---
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -0,0 +1,18 @@
+package edu.eci.arsw.primefinder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PrimeFinderThread pft=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000001, 20000000);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000001, 30000000);
+
+		pft.start();
+		pft2.start();
+		pft3.start();
+		
+		
+	}
+	
+}
